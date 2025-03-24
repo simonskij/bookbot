7687f99dbd454ef7967b11865a523b91b2f8d442
John Simonski <simonski.john@gmail.com>
2025-03-23 23:02:27 -0400

    get the book

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+path_to_book = "/home/silentspecter/Workspace/github.com/simonskij/bookbot/books/frankenstein.txt"
+
+def get_book_text(path_to_book):
+    with open(path_to_book) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    print(get_book_text(path_to_book))
+
+main()
