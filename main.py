4df842daba939811a2a65f7a14684a2e0d2f7d78
John Simonski <simonski.john@gmail.com>
2025-03-24 22:37:09 -0400

    add character counter

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ def get_book_text(path_to_book):
     return file_contents
 
 from stats import count_words
+from stats import get_character_dict
 
 def main():
     book_text = get_book_text(path_to_book)
     word_count = count_words(book_text)
+    character_dict = get_character_dict(book_text)
     print(f"{word_count} words found in the document")
-
+    print(character_dict)
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def count_words(text):
     words = text.split()
     return len(words)
+
+
+def get_character_dict(text):
+    character_dict = {}
+    words = text.split()
+    for word in words:
+        word = word.lower()
+        for letter in word:
+            if letter in character_dict:
+                character_dict[letter] += 1
+            else:
+                character_dict[letter] = 1
+    return character_dict   
